04f9d097007cf9ff37f220126df364f73fbf8f86
Yu Wei Chang <yuwei.chang@xuenn.com>
2021-03-02 14:12:12 +0800

    Feat: Add Session ReadOnly test

--- DotNet.Framework.Web/Controllers/SessionReadOnlyController.cs
+++ DotNet.Framework.Web/Controllers/SessionReadOnlyController.cs
@@ -1,19 +1,26 @@
+using System.Threading;
 using System.Web.Mvc;
 using System.Web.SessionState;
+using log4net;
 
 namespace DotNet.Framework.Web.Controllers
 {
     [SessionState(SessionStateBehavior.ReadOnly)]
     public class SessionReadOnlyController : Controller
     {
+        private ILog Logger => LogManager.GetLogger(this.GetType());
+        
         // GET
         public ActionResult Index()
         {
-            if (Session["count"] == null)
-                Session["count"] = 1;
-            else
-                Session["count"] = int.Parse(Session["count"].ToString()) + 1;
+            // if (Session["count"] == null)
+            //     Session["count"] = 1;
+            // else
+            //     Session["count"] = int.Parse(Session["count"].ToString()) + 1;
             
+            Logger.Info("Start");
+            Thread.Sleep(1000);
+            Logger.Info("End");
             return View();
         }
     }
